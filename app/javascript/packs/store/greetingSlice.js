87ac7fc40388a0ccdc2c045b117ce93d093a22e3
kit0-0 <kt1767@gmail.com>
2023-10-30 06:34:32 +0530

    Create a file for the action

---
+++ app/javascript/packs/store/greetingSlice.js
@@ -0,0 +1,22 @@
+// app/javascript/packs/store/greetingSlice.js
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchGreeting = createAsyncThunk('greeting/fetchGreeting', async () => {
+  const response = await axios.get('/api/greeting');
+  return response.data;
+});
+
+const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState: { value: '' },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreeting.fulfilled, (state, action) => {
+        state.value = action.payload.greeting;
+      });
+  },
+});
+
+export default greetingSlice.reducer;

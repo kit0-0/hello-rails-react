3f2053c0160a7239c1390fd7d74880c318ad5ddc
kit0-0 <kt1767@gmail.com>
2023-10-30 06:35:20 +0530

    Create a file for the store

---
+++ app/javascript/packs/store/store.js
@@ -0,0 +1,11 @@
+// app/javascript/packs/store/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import greetingReducer from './greetingSlice';
+
+const store = configureStore({
+  reducer: {
+    greeting: greetingReducer,
+  },
+});
+
+export default store;
